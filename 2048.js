9dbb2db12b4b9248c1ec06ad0db34ac1e13d4421
madiks <madiks@163.com>
2015-06-02 22:57:57 +0800

    add slide method

--- 2048.js
+++ 2048.js
@@ -74,32 +74,124 @@ display(gameData);
 function slideTo(direction, gameData){
   switch(direction){
     case 'top':
-
+      var gd = slideToTop(gameData);
     break;
 
     case 'bottom':
+      var gd = slideToBottom(gameData);
     break;
 
     case 'left':
+      var gd = slideToLeft(gameData);
     break;
 
     case 'right':
+      var gd = slideToRight(gameData);
     break;
   }
-  return gameData;
+  return gd;
 }
 
-function slideToTop(gameData){
-  console.log('top');
-  for(var y = 1; y < gameData.length; y++){
-    console.log(gameData[y]);
-    console.log('---');
-    for (var x = 0; x < gameData[y].length; x++) {
-
+function slideToTop(gd){
+  //var gd = MirrorV(gd);
+  //gd = MirrorV(gd);
+  var MapLen = gd.length;
+  for(var y = 1; y < MapLen; y++){
+    //console.log(gd[y]);
+    for (var x = 0; x < MapLen; x++) {
+      //console.log(gd[y][x]);
+      var tmpY = y;
+      while(tmpY > 0){
+        if(gd[tmpY][x] !== 0){
+          if(gd[tmpY][x] === gd[tmpY-1][x]){
+            gd[tmpY-1][x] = 2 * gd[tmpY][x];
+            gd[tmpY][x] = 0;
+          }else if(gd[tmpY-1][x] === 0){
+            gd[tmpY-1][x] = gd[tmpY][x];
+            gd[tmpY][x] = 0;
+          }
+        }
+        tmpY--;
+      }
     };
   }
-  return gameData;
+  return gd;
 }
 
-var gameData = slideToTop(gameData);
+function slideToBottom(gameData) {
+  var gd = MirrorV(gameData);
+  gd = slideToTop(gd);
+  gd = MirrorV(gd);
+  return gd;
+}
+
+function slideToLeft(gameData){
+  var gd = Right90(gameData);
+  gd = slideToTop(gd);
+  gd = Left90(gd);
+  return gd;
+}
+
+function slideToRight(gameData){
+  var gd = Left90(gameData);
+  gd = slideToTop(gd);
+  gd = Right90(gd);
+  return gd;
+}
+
+function Right90(gameData){
+  var MapLen = gameData.length;
+  var gd = init(MapLen);
+  gameData.forEach(function(row, x){
+    row.forEach(function(elem, y){
+      gd[y][MapLen-x-1] = elem;
+    });
+  })
+  return gd;
+}
+
+function Left90(gameData) {
+  var MapLen = gameData.length;
+  var gd = init(MapLen);
+  gameData.forEach(function(row, x){
+    row.forEach(function(elem, y){
+      gd[MapLen-y-1][x] = elem;
+    });
+  })
+  return gd;
+}
+
+function MirrorV(gameData) {
+  var MapLen = gameData.length;
+  var gd = init(MapLen);
+  gameData.forEach(function(row, x){
+    row.forEach(function(elem, y){
+      gd[MapLen-x-1][y] = elem;
+    });
+  })
+  return gd;
+}
+
+console.log('source:')
 display(gameData);
+var gd = slideTo('top', gameData);
+console.log('slideTo top');
+display(gd);
+
+console.log('source:')
+display(gameData);
+var gd = slideTo('bottom', gameData);
+console.log('slideTo bottom');
+display(gd);
+
+console.log('source:')
+display(gameData);
+var gd = slideTo('left', gameData);
+console.log('slideTo left');
+display(gd);
+
+console.log('source:')
+display(gameData);
+var gd = slideTo('right', gameData);
+console.log('slideTo right');
+display(gd);
